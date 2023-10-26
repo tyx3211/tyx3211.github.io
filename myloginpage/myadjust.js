e02ac63f3edaadead082f23f2b3f0b1408e5cb61
tyx3211 <145897809+tyx3211@users.noreply.github.com>
2023-10-26 13:02:10 +0800

    Add files via upload

---
+++ myloginpage/myadjust.js
@@ -0,0 +1,3 @@
+let myView=document.querySelector("#container");
+myView.style.cssText+="height:"+String(window.innerHeight)+"px !important;"
+myView.style.cssText+="width:"+String(window.innerWidth)+"px !important;"
