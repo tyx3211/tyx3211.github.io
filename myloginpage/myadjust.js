9473f0e3e3b474ffd078ba6927df935cf01298bb
tyx3211 <145897809+tyx3211@users.noreply.github.com>
2023-10-27 13:30:35 +0800

    Delete myloginpage/myadjust.js

--- myloginpage/myadjust.js
+++
@@ -1,3 +0,0 @@
-let myView=document.querySelector("#container");
-myView.style.cssText+="height:"+String(window.innerHeight)+"px !important;"
-myView.style.cssText+="width:"+String(window.innerWidth)+"px !important;"
